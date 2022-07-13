f3bd9886d6d594dc2b0dae565f6d583588a72255
Marcos Magaña Hernández <imarcosmaganah@gmail.com>
2022-07-13 00:14:38 -0500

    se agrega el model category

--- inventory-backend/src/main/java/com/markcode/inventory/app/model/Category.java
+++ inventory-backend/src/main/java/com/markcode/inventory/app/model/Category.java
@@ -1,2 +1,28 @@
-package com.markcode.inventory.app.model;public class Category {
+package com.markcode.inventory.app.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="Category")
+@Data
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "ID_CATEGORY", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @Column(name = "DESCRIPTION", nullable = false)
+    private String description;
+
+
+
 }
