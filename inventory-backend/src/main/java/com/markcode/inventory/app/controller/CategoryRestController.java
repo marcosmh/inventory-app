99497c973c801feafa129e077b439bdf8fe99a43
Marcos Magaña Hernández <imarcosmaganah@gmail.com>
2022-07-13 23:05:43 -0500

    configuracion de cors

--- inventory-backend/src/main/java/com/markcode/inventory/app/controller/CategoryRestController.java
+++ inventory-backend/src/main/java/com/markcode/inventory/app/controller/CategoryRestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class CategoryRestController {
 
     @Autowired
